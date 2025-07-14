e2dfa9850fc507072d79177aea98b2a8c38e41db
Valentinus Delvin <delvinwicaksono@gmail.com>
2025-07-14 23:30:52 +0700

    update(home): add description for todos apps and expense tracker

--- src/helpers/HomeHelper.jsx
+++ src/helpers/HomeHelper.jsx
@@ -6,24 +6,24 @@ export const tools = [
     {
       title: "To-Do List",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+        "A simple todo-list app to track, manage, and complete your daily tasks.",
       icon: CheckSquare,
       path: "/todolist",
       color: "text-primary",
       bgColor: "bg-primary/10",
       available: true,
-      features: ["Cihuyyy", "Cihuyyy", "Cihuyyy", "Cihuyyy"],
+      features: ["Add new tasks with descriptions", "Mark tasks as completed or active", "Filter tasks by All, Active, or Completed", "Display total, active, and completed task statistics"],
     },
     {
       title: "Expense Tracker",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.",
+        "A simple money manager app to track and manage your daily expenses.",
       icon: DollarSign,
       path: "/expense-tracker",
       color: "text-success",
       bgColor: "bg-success/10",
-      available: false,
-      features: ["Cihuyy", "Cihuyy", "Cihuyy", "Cihuyy"],
+      available: true,
+      features: ["Add new transactions with description and amount", "Categorize transactions as Income or Expense", "Display total income, total expenses, and remaining balance", "Filter transaction history by All, Income, or Expense"],
     },
     {
       title: "Health Tracker",
